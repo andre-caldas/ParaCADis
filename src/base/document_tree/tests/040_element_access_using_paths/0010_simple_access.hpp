09abeb18a360b769e09ecfca480a25c6b3b8f2c5
André Caldas <andre.em.caldas@gmail.com>
2024-04-17 18:53:01 -0300

    Tests expected fails.

--- src/base/document_tree/tests/040_element_access_using_paths/0010_simple_access.hpp
+++ src/base/document_tree/tests/040_element_access_using_paths/0010_simple_access.hpp
@@ -89,20 +89,24 @@ SCENARIO("Accessing a container element using a path", "[simple]")
             NamingScheme::ReferenceTo<Real> px_ref{a, pname, "x"};
             NamingScheme::ReferenceTo<Real> py_ref{a, pname, "y"};
             NamingScheme::ReferenceTo<Real> pz_ref{a, pname, "z"};
+            NamingScheme::ReferenceTo<Real> pbad_ref{a, pname, "x bad"};
 
             NamingScheme::ReferenceTo<Real> vx_ref{a, vname, "x"};
             NamingScheme::ReferenceTo<Real> vy_ref{a, vname, "y"};
             NamingScheme::ReferenceTo<Real> vz_ref{a, vname, "z"};
+            NamingScheme::ReferenceTo<Real> vbad_ref{a, vname, "y bad"};
 
             NamingScheme::ReferenceTo<Real> lsx_ref{a, lname, "start", "x"};
             NamingScheme::ReferenceTo<Real> lsy_ref{a, lname, "start", "y"};
             NamingScheme::ReferenceTo<Real> lsz_ref{a, lname, "start", "z"};
-
             NamingScheme::ReferenceTo<Real> ldx_ref{a, lname, "direction", "x"};
+            NamingScheme::ReferenceTo<Real> lbad_ref{a, vname, "start"};
 
             NamingScheme::ReferenceTo<Real> ccx_ref{a, cname, "center", "x"};
             NamingScheme::ReferenceTo<Real> cnx_ref{a, cname, "normal", "x"};
             NamingScheme::ReferenceTo<Real>  cr_ref{a, cname, "radius"};
+            NamingScheme::ReferenceTo<DeferenceablePoint>
+                                           cbad_ref{a, lname, "center", "x"};
 
             THEN("we get working references")
             {
@@ -117,10 +121,31 @@ SCENARIO("Accessing a container element using a path", "[simple]")
               REQUIRE(px == *(ReaderGateKeeper{lsx_ref.resolve()}));
               REQUIRE(py == *(ReaderGateKeeper{lsy_ref.resolve()}));
               REQUIRE(pz == *(ReaderGateKeeper{lsz_ref.resolve()}));
+              REQUIRE(vx == *(ReaderGateKeeper{ldx_ref.resolve()}));
 
               REQUIRE(px == *(ReaderGateKeeper{ccx_ref.resolve()}));
               REQUIRE(vx == *(ReaderGateKeeper{cnx_ref.resolve()}));
               REQUIRE(radius == *(ReaderGateKeeper{cr_ref.resolve()}));
+            }
+
+            THEN("bad references evaluate to false")
+            {
+              REQUIRE_FALSE(pbad_ref.resolve());
+              REQUIRE_FALSE(vbad_ref.resolve());
+              REQUIRE_FALSE(lbad_ref.resolve());
+              REQUIRE_FALSE(cbad_ref.resolve());
+            }
+
+            THEN("bad references throw when accessed")
+            {
+              REQUIRE_THROWS_AS(*(ReaderGateKeeper{pbad_ref.resolve()}),
+                                std::bad_optional_access);
+              REQUIRE_THROWS_AS(*(ReaderGateKeeper{vbad_ref.resolve()}),
+                                std::bad_optional_access);
+              REQUIRE_THROWS_AS(*(ReaderGateKeeper{lbad_ref.resolve()}),
+                                std::bad_optional_access);
+              REQUIRE_THROWS_AS(*(ReaderGateKeeper{cbad_ref.resolve()}),
+                                std::bad_optional_access);
             }
           }
         }
