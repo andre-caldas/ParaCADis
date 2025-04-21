2d958cf8470322e927787f5b8743751158c45d72
André Caldas <andre.em.caldas@gmail.com>
2025-04-20 21:13:26 -0300

    Migrating to build2.

--- buildfile
+++ buildfile
@@ -1,2 +1,6 @@
-./: cpp_modules_wrapper/ \
-  libparacadis/
+# Glue buildfile that "pulls" all the packages in the project.
+#
+import pkgs = [dir_paths] $process.run_regex(\
+  cat $src_root/packages.manifest, '\s*location\s*:\s*(\S+)\s*', '\1')
+
+./: $pkgs
---
+++ cpp_module_wrappers/libcgal/cgal.cxx
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+/****************************************************************************
+ *                                                                          *
+ *   Copyright (c) 2025 André Caldas <andre.em.caldas@gmail.com>            *
+ *                                                                          *
+ *   This file is part of ParaCADis.                                        *
+ *                                                                          *
+ *   ParaCADis is free software: you can redistribute it and/or modify it   *
+ *   under the terms of the GNU General Public License as published         *
+ *   by the Free Software Foundation, either version 2.1 of the License,    *
+ *   or (at your option) any later version.                                 *
+ *                                                                          *
+ *   ParaCADis is distributed in the hope that it will be useful, but       *
+ *   WITHOUT ANY WARRANTY; without even the implied warranty of             *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.                   *
+ *   See the GNU General Public License for more details.                   *
+ *                                                                          *
+ *   You should have received a copy of the GNU General Public License      *
+ *   along with ParaCADis. If not, see <https://www.gnu.org/licenses/>.     *
+ *                                                                          *
+ ***************************************************************************/
+
+module;
+#include <CGAL/number_utils.h>
+module cgal.types;
+
+namespace cgal {
+  float sqrt_float(const Real& x)
+  {
+    return static_cast<float>(CGAL::to_double(CGAL::sqrt(x)));
+  }
+}
